c3d234ebf585d2c8454e5db86279858ba7ac3b0b
TenochV <90113344+TenochV@users.noreply.github.com>
2021-09-04 14:44:43 -0700

    Create Hello_leia.py
    english:
    this program calculates the name length and did you notice the variables caca + pipi = poop and pee lol
    
    spanish
    este programa calcula la longitud del nomble lol notaron los nombres son caca y pipi

---
+++ Hello_leia.py
@@ -0,0 +1,7 @@
+name = input('enter name')
+last_name = input('enter last name')
+print('hello ' + name + ' ' + last_name)
+caca = len(name)
+pipi = len(last_name)
+# LOL
+print('your name length is ' + str(caca + pipi) )
