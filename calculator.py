26ff57ceac05f30757d1f200269a58947c0f26d3
TenochV <90113344+TenochV@users.noreply.github.com>
2021-09-12 11:00:41 -0700

    Create calculator.py
    this calculates the numbers you insert
    
    esto calcula kk

---
+++ calculator.py
@@ -0,0 +1,18 @@
+numero_1 = input ("inserta un numero:")
+numero_2 = input ("inseta otro numero:")
+
+print('numero 1: es '+numero_1)
+print('numero 2: es '+numero_2)
+
+entero_1 = int(numero_1)
+entero_2 = int(numero_2)
+
+suma = entero_1 + entero_2
+resta = entero_1 - entero_2
+multiplication = entero_1 * entero_2
+division = entero_1 / entero_2
+
+print('suma=', suma)
+print('resta=', resta)
+print('multiplication=', multiplication)
+print('division=', division)
