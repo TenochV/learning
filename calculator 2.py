0d5ac5a81b9cd102e022ddf2fd00cfbbdd093701
TenochV <90113344+TenochV@users.noreply.github.com>
2021-09-12 11:18:31 -0700

    Create calculator 2.py
    its the same thing as calculator.py but it calculates 3 numbers
    
    es la misma cosa que calculator.py pero calcula 3 kk (kk es por popo)

---
+++ calculator 2.py
@@ -0,0 +1,17 @@
+numero_1 = input ("inserta un numero:")
+numero_2 = input ("inseta otro numero:")
+numero_3 = input ("inserta otro numero")
+
+print('numero 1: es '+numero_1)
+print('numero 2: es '+numero_2)
+print('numero 3: es '+numero_3)
+
+entero_1 = int(numero_1)
+entero_2 = int(numero_2)
+entero_3 = int(numero_3)
+
+suma = entero_1 + entero_2 + entero_3
+resta = entero_1 - entero_2 - entero_3
+
+print('suma=', suma)
+print('resta=', resta)
