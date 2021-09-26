79402f9768fa67fca11fc82ad7b7e212335ba93e
TenochV <90113344+TenochV@users.noreply.github.com>
2021-09-26 16:09:21 -0700

    Create hellome.py
    this tells your what you should wear in your legs 
    but i didn't do for the shirts so you choose them yourself

---
+++ hellome.py
@@ -0,0 +1,8 @@
+if input("Va a llover? Si/No") == "si": 
+    print("Lleva sombrilla")
+elif input("Va hacer frio?") == "si":
+    print("Lleva sudadera")
+elif input("Va a hacer calor?") == "si":
+    print("Lleva shorts")
+else:
+    print("Lleva pantalon")
