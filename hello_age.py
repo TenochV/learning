37b7339da74d79776b7f96e44ab0877bfca85ab5
TenochV <90113344+TenochV@users.noreply.github.com>
2021-10-02 20:41:42 -0700

    Create hello_age.py

---
+++ hello_age.py
@@ -0,0 +1,27 @@
+amogus = input('pon un nombre:')
+sus = input('pon una edad')
+
+poop = int(sus)
+
+if poop<3:
+   print(amogus,"es bebe")
+   
+elif poop<12:
+    print(amogus, "es niño")
+   
+elif poop<17:
+    print(amogus, "es adolesente")
+    
+elif poop<60:
+    print(amogus, "es adulto")
+    
+elif poop<99:
+    print(amogus, "es viejo")
+    
+elif poop<110:
+    print(amogus, "es ultra viejo")
+    
+elif poop<900000:
+    print (amogus, "ta muerto :(")
+
+#lololololpololollololololollolololololololololololo :)  O_O
